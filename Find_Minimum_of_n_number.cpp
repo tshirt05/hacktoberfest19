f351fdc3073efa2f500c367ad003c979612948c7
tshirt05 <57018548+tshirt05@users.noreply.github.com>
2019-10-26 02:07:19 +0530

    currecting the code

--- Find_Minimum_of_n_number.cpp
+++ Find_Minimum_of_n_number.cpp
@@ -1,20 +1,21 @@
-#include<stdio.h>
+#include<iostream>
 using namespace std;
 int main(){
 	int n,i,min;
-	scanf("%d",&n);
+	cin>>n;
 	int a[n];
 	for(i=0;i<n;i++)
 	{
-	scanf("%d",&a[i]);
+	cin>>a[i];
 	}
 	min=a[0];
 	for(i=0;i<n;i++)
 	{
 		if(min>a[i])
-		{rry
+		{
+			min=a[i];
 		}
 	}
-	printf("%d",min);
+	cout<<min;
 	
 }
